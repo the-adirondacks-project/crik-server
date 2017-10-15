ba37bda329f18432977e1960ba675eeaf49de587
Jacob Leach <leach.jacob@gmail.com>
2017-10-15 15:35:34 -0400

    Add schema for database

---
+++ schema.sql
@@ -0,0 +1,48 @@
+CREATE TABLE video_files (
+    id integer NOT NULL,
+    url text NOT NULL,
+    video_id integer NOT NULL
+);
+
+CREATE SEQUENCE video_files_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE video_files_id_seq OWNED BY video_files.id;
+
+CREATE SEQUENCE video_libraries_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE videos (
+    id integer NOT NULL,
+    name text NOT NULL
+);
+
+CREATE SEQUENCE videos_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE videos_id_seq OWNED BY videos.id;
+
+ALTER TABLE ONLY video_files ALTER COLUMN id SET DEFAULT nextval('video_files_id_seq'::regclass);
+
+ALTER TABLE ONLY videos ALTER COLUMN id SET DEFAULT nextval('videos_id_seq'::regclass);
+
+ALTER TABLE ONLY video_files
+    ADD CONSTRAINT video_files_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY videos
+    ADD CONSTRAINT videos_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY video_files
+    ADD CONSTRAINT video_files_video_id_fkey FOREIGN KEY (video_id) REFERENCES videos(id);
