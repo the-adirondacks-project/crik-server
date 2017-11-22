b8c5ee058ab11104f9a72af0259761162966106e
Jacob Leach <leach.jacob@gmail.com>
2017-11-22 17:24:14 -0500

    Refactor the API types to make it more readable

--- src/API.hs
+++ src/API.hs
@@ -19,32 +19,46 @@ import Types.VideoLibrary (VideoLibrary, VideoLibraryId)
 
 type API = VideoAPI :<|> VideoLibraryAPI
 
-type VideoAPI = "videos" :> (
-    Get '[JSON] [Video VideoId] :<|>
-    ReqBody '[JSON] (Video NoId) :> Post '[JSON] (Video VideoId) :<|>
-    Capture "videoId" Int :> ReqBody '[JSON] (Video NoId) :> Put '[JSON] (Video VideoId) :<|>
-    Capture "videoId" Int :> Get '[JSON] (Video VideoId) :<|>
-    Capture "videoId" Int :> "files" :> Get '[JSON] [VideoFile] :<|>
-    Capture "videoId" Int :> "files" :> Capture "videoFileId" Int :> Get '[JSON] VideoFile :<|>
-    "files" :> (
-      Get '[JSON] [VideoFile] :<|>
-      Capture "videoFileId" Int :> Get '[JSON] VideoFile
-    )
-  )
+type VideoAPI =
+  GetVideos :<|>
+  CreateVideo :<|>
+  UpdateVideo :<|>
+  GetVideo :<|>
+  CaptureVideoId :> GetFiles :<|>
+  CaptureVideoId :> GetFile :<|>
+  GetFiles :<|>
+  GetFile
+
+type Videos = "videos"
+type CaptureVideoId = Capture "videoId" Int
+
+type GetVideos = Videos :> Get '[JSON] [Video VideoId]
+type GetVideo = Videos :> CaptureVideoId :> Get '[JSON] (Video VideoId)
+type CreateVideo = Videos :> ReqBody '[JSON] (Video NoId) :> Post '[JSON] (Video VideoId)
+type UpdateVideo = Videos :> CaptureVideoId :> ReqBody '[JSON] (Video NoId) :> Put '[JSON] (Video VideoId)
+
+type Files = "files"
+type CaptureFileId = Capture "videoFileId" Int
+
+type GetFiles = Files :> Get '[JSON] [VideoFile]
+type GetFile = Files :> CaptureFileId :> Get '[JSON] VideoFile
 
 type VideoLibraryAPI =
-    GetVideoLibraries :<|>
-    GetVideoLibrary :<|>
-    GetNewFilesInVideoLibrary :<|>
-    GetAllFilesInVideoLibrary :<|>
-    CreateVideoLibrary :<|>
-    UpdateVideoLibrary
+  GetVideoLibraries :<|>
+  GetVideoLibrary :<|>
+  GetNewFilesInVideoLibrary :<|>
+  GetAllFilesInVideoLibrary :<|>
+  CreateVideoLibrary :<|>
+  UpdateVideoLibrary
 
-type GetVideoLibraries = "video_libraries" :> Get '[JSON] [VideoLibrary VideoLibraryId]
-type GetVideoLibrary = "video_libraries" :> Capture "videoLibraryId" Int :> Get '[JSON] (VideoLibrary VideoLibraryId)
-type GetNewFilesInVideoLibrary = "video_libraries" :> Capture "videoLibraryId" Int :> "new_files" :> Get '[JSON] [Text]
-type GetAllFilesInVideoLibrary = "video_libraries" :> Capture "videoLibraryId" Int :> "all_files" :> Get '[JSON] [Text]
-type CreateVideoLibrary = "video_libraries" :> ReqBody '[JSON] (VideoLibrary NoId) :>
+type VideoLibraries = "video_libraries"
+type CaptureVideoLibraryId = Capture "videoLibraryId" Int
+
+type GetVideoLibraries = VideoLibraries :> Get '[JSON] [VideoLibrary VideoLibraryId]
+type GetVideoLibrary = VideoLibraries :> CaptureVideoLibraryId :> Get '[JSON] (VideoLibrary VideoLibraryId)
+type GetNewFilesInVideoLibrary = VideoLibraries :> CaptureVideoLibraryId :> "new_files" :> Get '[JSON] [Text]
+type GetAllFilesInVideoLibrary = VideoLibraries :> CaptureVideoLibraryId :> "all_files" :> Get '[JSON] [Text]
+type CreateVideoLibrary = VideoLibraries :> ReqBody '[JSON] (VideoLibrary NoId) :>
   Post '[JSON] (VideoLibrary VideoLibraryId)
-type UpdateVideoLibrary = "video_libraries" :> Capture "videoLibraryId" Int :> ReqBody '[JSON] (VideoLibrary NoId) :>
+type UpdateVideoLibrary = VideoLibraries :> CaptureVideoLibraryId :> ReqBody '[JSON] (VideoLibrary NoId) :>
   Put '[JSON] (VideoLibrary VideoLibraryId)
